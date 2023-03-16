0e303c740a2c5ca5df9895727c17ad33994cb0d8
AYOUB00HUB <xa3503993@gmail.com>
2023-03-16 15:28:37 -0700

    my

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
-int main(void)
-{
-
 /**
  * main - Entry poit
  *
  * Description: A C program that print with printf function.
  *
  * Return: Always 0 (Success)
-*/
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
