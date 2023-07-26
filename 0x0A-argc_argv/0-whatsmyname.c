7a4cf0b49647309c0569d3ac94add61f42e34842
AYOUB00HUB <xa3503993@gmail.com>
2023-07-26 11:15:20 -0700

    m

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function main
+ * @argc: counter
+ * @argv: array of arguments
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
